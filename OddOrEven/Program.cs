1f89cd01b9552b8327382e104575c5d8d4b0c65a
Marcus Piiritalo <marcus.piiritalo@gmail.com>
2023-02-03 13:15:56 +0200

    add Week3IfAndSwitch code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.WriteLine("Enter a number:");
+int UserNum = Convert.ToInt32(Console.ReadLine());
+
+//kas kasutaja arv on paaris voi paaritu
+
+int result = UserNum % 2;
+
+if (result != 0) //!= - ei ole
+{
+    Console.WriteLine("User numbebr is odd.");
+}else
+{
+    Console.WriteLine("User number is even.");
+}
---
+++ Week3ifAndSwitch/Program.cs
@@ -0,0 +1,9 @@
+﻿//modulus - %
+
+int a = 25;
+int b = 8;
+
+int c = b % a; //+/-/* - /
+
+Console.WriteLine(c);
+
